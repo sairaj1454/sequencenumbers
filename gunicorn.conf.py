176922132733355813d1601a518f5e03064cb5a5
sairajdeep <sairaj.thikkisetty@perficient.com>
2025-08-14 15:58:18 +0530

    y

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,11 @@
+import os
+
 # gunicorn configuration
 workers = 4
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 30
 keepalive = 2
-bind = '0.0.0.0:' + str(int('$PORT')) if '$PORT' in os.environ else '0.0.0.0:5000'
+
+# Get port from environment variable or use default
+bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
